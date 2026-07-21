fd9d62ffdfc837844574c937601cec9dccd70dbd
BAKHOUM21 <bakhoumdemba1@gmail.com>
2026-07-21 14:05:13 +0000

    Second commit

--- src/app/detail-annonces/detail-annonces.component.ts
+++ src/app/detail-annonces/detail-annonces.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http';
-import {IonHeader,IonTitle,IonToolbar,IonContent,IonBackButton} from '@ionic/angular/standalone';
+import {IonHeader,IonTitle,IonToolbar,IonContent,IonBackButton,IonCardContent,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCard} from '@ionic/angular/standalone';
 @Component({
   selector: 'app-detail-annonces',
   templateUrl: './detail-annonces.component.html',
   styleUrls: ['./detail-annonces.component.scss'],
-  imports:[IonHeader,IonTitle,IonToolbar,IonContent,IonBackButton]
+  imports:[IonHeader,IonTitle,IonToolbar,IonContent,IonBackButton,IonCardContent,IonCardHeader,IonCard]
 })
 export class DetailAnnoncesComponent {
-  annonce= [];
+  annonce!: any;
   constructor(private http: HttpClient) {
     let id_ann = localStorage.getItem('id_ann');
-    let headers=new HttpHeaders({
-      'Content-Type': 'application/json',
-      "Autorization":`Bearer ${localStorage.getItem('token')}`
-    });
-    this.http.get('http://51.210.43.90/api/annonce/' + id_ann,{headers}).subscribe((data: any) => {
-       this.annonce = data;
-       console.log(data);
+   const headers= new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+      });
+    this.http.get('http://51.210.43.90/api/annonce/' + id_ann, { headers }).subscribe((data: any) => {
+       this.annonce=data.annonce ;
+       console.log(this.annonce);
     });
    }
 
